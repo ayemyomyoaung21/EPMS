b8fee57a1e0684fecf992c6ef49e09d55b5fcf49
Aye Myo Myo Aung <ayemyomyoaung17@gmail.com>
2026-04-14 01:35:39 +0630

    added correct frontend folder structure

--- react_epms/src/main.tsx
+++ react_epms/src/main.tsx
@@ -4,11 +4,12 @@ import './index.css'
 import App from './App.tsx'
 import { Provider } from 'react-redux'
 import { store } from './app/store.ts'
+
 createRoot(document.getElementById('root')!).render(
-  
   <StrictMode>
     <Provider store={store}>
-    <App />
-    </Provider>
+       <App />
+      </Provider>
+   
   </StrictMode>,
 )
